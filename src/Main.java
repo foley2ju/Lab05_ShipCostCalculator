bfcaf18fe3d2caea890b7dbccfafc9b921e0504a
Josh <foley2ju@mail.uc.edu>
2024-02-14 13:27:23 -0500

    Lab05_ShipCostCalculator

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+
+    {
+        double itemPrice = 0.0;
+        double shippingCost = 0.0;
+        double SHIPPING_RATE = 0.02;
+        double totalPrice = 0.0;
+        int FREE_SHIPPING = 100;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter item price: ");
+        itemPrice = in.nextDouble();
+
+        if (itemPrice > FREE_SHIPPING) {
+
+            System.out.println("Your shipping cost is free!");
+            System.out.println("Your total price is " + itemPrice);
+
+        } else {
+
+            shippingCost = itemPrice * SHIPPING_RATE;
+            totalPrice = itemPrice + shippingCost;
+            System.out.println("Your shipping price is " + shippingCost);
+            System.out.println("Your total price is " + totalPrice);
+        }
+    }
+}
